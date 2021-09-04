a09eeee8693637dbc598f254d237aec23ddbdfec
Luis Garcia <lalejandro.1988@gmail.com>
2021-09-04 12:44:32 -0600

    Estructura basica Rutas App

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon List</router-link>
+        <router-link to="/id">Pokemon por ID</router-link>
+        <router-link to="/about">About</router-link>
+    </div>    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+    { 
+        path: '/',
+        component: () => import( /* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage' ) 
+    },
+    { 
+        path: '/about',
+        component: () => import( /* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage' )
+    },
+    { 
+        path: '/id',
+        component: () => import( /* webpackChunkName: "DetailPage" */ '@/modules/pokemon/pages/DetailPage' ) 
+    },
+    { 
+        path: '/:pathMatch(.*)*',
+        component: () => import( /* webpackChunkName: "NoPageFound" */ '@/modules/shared/pages/NoPageFound' ) 
+    },
+]
+
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+export default router
